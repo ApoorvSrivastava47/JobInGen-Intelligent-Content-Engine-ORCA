35665deef6f492bae6ad68d3bde8315ca60f2d63
Apoorv <eng.apoorv@gmail.com>
2026-07-02 23:48:44 +0530

    Fix image URL

--- backend/app/main.py
+++ backend/app/main.py
@@ -13,7 +13,7 @@ app.add_middleware(
     CORSMiddleware,
     allow_origins=[
         "http://localhost:5173",
-        "https://job-in-gen-intelligent-content-engine-orca-60yu7gt61-orc-a.vercel.app",
+        "https://job-in-gen-intelligent-content-engine-orca-nj8ciu631-orc-a.vercel.app",
     ],
     allow_credentials=True,
     allow_methods=["*"],
